b6d1b0624ac98dcdd52f11cf0baf567c822aa230
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-05-04 14:54:55 -0400

    Add temp tasks data

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,35 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctor's Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "Feb 5th at 2:30pm",
-    reminder: false,
-  }
-]
+import { useState } from "react";
 
 const Tasks = () => {
-  return (
-    <div>
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    }
+  ])
 
-    </div>
+  return (
+    <>
+      {
+        tasks.map((task) => (
+          <h3 key={task.id}>{task.text}</h3>
+        ))
+      }
+    </>
   )
 }
 
